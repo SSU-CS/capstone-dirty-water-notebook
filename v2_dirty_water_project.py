d82c2ef98500a8dac616f35a4b585b1e9262743c
H-Elizabeth <elizabeth__hudson@outlook.com>
2024-11-17 20:25:06 -0800

    Update v2_dirty_water_project.py

--- v2_dirty_water_project.py
+++ v2_dirty_water_project.py
@@ -923,4 +923,4 @@ def update_map(selected_date_index, color_value, relayout_data, lat_lon, current
     return fig, lat_lon, zoom_level
 
 
-app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+app.run_server(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
